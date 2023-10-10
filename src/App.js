a12ec32c960388e0146a8d6db3081781e3e64edc
Pravv13 <gpravasta13@gmail.com>
2023-10-10 19:50:52 +0700

    feat: Add read function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Crud from "./CRUD";
+import 'bootstrap/dist/css/bootstrap.min.css';;
 
 function App() {
   return (
--- src/CRUD.js
+++ src/CRUD.js
@@ -1,4 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { Card } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+import { BiSolidEditAlt } from 'react-icons/bi';
+import { MdDeleteOutline } from 'react-icons/md'
 
 function Crud() {
 const [create, setCreate] = useState([])
@@ -21,31 +25,63 @@ const handleCreate = () => {
 console.log({create})
   return (
     <div>
-        <h4>Nama:</h4>
-        <input 
-        type='text'
-        onChange={(e) => setinputNama(e.target.value)}
-        value={inputNama}
-        placeholder='Masukkan Nama Anda'
-        />
-        <h4>NIM:</h4>
-        <input
-        type='text'
-        onChange={(e) => setInputNim(e.target.value)}
-        value={inputNim}
-        placeholder='Masukkan NIM Anda'       
-        /><br></br>
-        <h4>Pilih Mata Kuliah:</h4>
-        <select name='Makul' id='Makul' value={inputMakul} onChange={(e) => setInputMakul(e.target.value)}>
-            <option value="">--Pilih Mata Kuliah--</option>
-            <option value="Dasar Pemrograman">Dasar Pemrograman</option>
-            <option value="Algoritma dan Struktur Data">Algoritma dan Struktur Data</option>
-            <option value="Logika Matematika">Logika Matematika</option>
-            <option value="Pemrograman Web">Pemrograman Web</option>
-            <option value="Statistika Data">Statistika Data</option>
-        </select>
-        <br></br><br></br>
-        <button onClick={handleCreate}>Tambah Data</button>
+    {/* start input create */}
+        <Card className='align-items-center mx-auto'>
+            <h4>Nama:</h4>
+            <input 
+            type='text'
+            onChange={(e) => setinputNama(e.target.value)}
+            value={inputNama}
+            placeholder='Masukkan Nama Anda'
+            />
+            <h4>NIM:</h4>
+            <input
+            type='text'
+            onChange={(e) => setInputNim(e.target.value)}
+            value={inputNim}
+            placeholder='Masukkan NIM Anda'       
+            /><br></br>
+            <h4>Pilih Mata Kuliah:</h4>
+            <select name='Makul' id='Makul' value={inputMakul} onChange={(e) => setInputMakul(e.target.value)}>
+                <option value="">--Pilih Mata Kuliah--</option>
+                <option value="Dasar Pemrograman">Dasar Pemrograman</option>
+                <option value="Algoritma dan Struktur Data">Algoritma dan Struktur Data</option>
+                <option value="Logika Matematika">Logika Matematika</option>
+                <option value="Pemrograman Web">Pemrograman Web</option>
+                <option value="Statistika Data">Statistika Data</option>
+            </select>
+            <br></br><br></br>
+            <button onClick={handleCreate}>Tambah Data</button>
+            <h1></h1>
+        </Card>
+    {/* end input create */}
+    {/* start read funciton */}
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Nama</th>
+          <th>NIM</th>
+          <th>Mata Kuliah</th>
+          <th>Edit</th>
+          <th>Hapus</th>
+        </tr>
+      </thead>
+      <tbody>
+      {create.map((item, index) => (
+            <tr key={item}>
+                <td>{item.id}</td>
+                <td>{item.text}</td>
+                <td>{item.nim}</td>
+                <td>{item.makul}</td>
+                <td><BiSolidEditAlt /></td>
+                <td><MdDeleteOutline /></td>
+            </tr>
+      ))
+      }
+      </tbody>
+    </Table>
+    {/* end read function */}
     </div>
   )
 }
